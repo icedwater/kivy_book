ea3f9cc5de48e9dc42ecd3b71196b75482e432f2
icedwater <icedwater@gmail.com>
2016-01-01 01:42:35 +0800

    Added experiments with layouts: basic, nested, and DPs. Also custom widgets.

---
+++ customwidget.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+"""
+Create a custom widget for use in future Kivy applications.
+"""
+
+from kivy.app import App
+
+class CustomWWeatherApp(App):
+    """
+    Here is the first iteration of the WeatherApp with a custom widget
+    defined in its .kv file.
+    """
+    pass
+
+if __name__ == "__main__":
+    CustomWWeatherApp().run()
---
+++ packedboxes.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+"""
+Here is a custom layout with nested packed boxlayouts. How will it work?
+"""
+
+from kivy.app import App
+
+class PackedBoxesApp(App):
+    """
+    Look in packedboxes.kv for the layout definition.
+    """
+    pass
+
+if __name__ == "__main__":
+    PackedBoxesApp().run()
